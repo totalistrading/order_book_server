dbd18c50994f4675feb294207ecab8663c98404a
Abhi Prajapati <38825389+abhi-praj@users.noreply.github.com>
2026-09-11 01:38:24 -0400

    Assert decoded source height and correct snapshot tuple documentation (#10)

--- server/src/listeners/order_book/state.rs
+++ server/src/listeners/order_book/state.rs
@@ -51,7 +51,7 @@ impl OrderBookState {
         (self.time, self.height, compute_l2_snapshots(&self.order_book))
     }
 
-    // (time, snapshot)
+    // (time, height, snapshot)
     pub(super) fn l2_snapshots(&mut self, prevent_future_snaps: bool) -> Option<(u64, u64, L2Snapshots)> {
         if self.snapped {
             None
